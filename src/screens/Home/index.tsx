e81a2b1a1fdc9d37af2501048ec78a95abf97c6d
Igor Zielosko <igorzielosko.iz@gmail.com>
2024-05-08 11:53:53 -0300

    Ajustes em regras

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,15 +1,28 @@
-import { Text, View, TextInput, TouchableOpacity } from "react-native";
+import { Text, View, TextInput, TouchableOpacity, FlatList, Alert } from "react-native";
 import { styles } from './styles'
 import { Participant } from "../components/Participant";
 
 export function Home(){ 
 
+  const participants = ['Igor', 'João', 'Daniel', 'Samuel', 'Tiago', 'Rafael', 'Vitor', 'Maria', 'Jaqueline', 'Matheus', 'José', 'Pedro', 'Paulo']
+
   async function handleParticipantAdd(){
-    
+    if (participants.includes('Igor')){
+      return Alert.alert('Participante existe', 'Já existe um participante na lista com esse nome')
+    }
   } 
 
   function handleParticipantRemove(name: string){
-    console.log(name)
+    Alert.alert('Remover', `Remover o participante ${name}?`, [
+      {
+        text: 'Sim',
+        onPress: () => Alert.alert('Deletado!')
+      },
+      {
+        text: 'Não',
+        style: 'cancel'
+      }
+    ])
   }
 
   return (
@@ -35,9 +48,23 @@ export function Home(){
       </TouchableOpacity>
     </View>
 
-    <Participant name={'Igor'} onRemove={() => handleParticipantRemove('Igor')}/>
-    {/* <Participant name={'Daniel'}/>
-    <Participant name={'João'}/> */}
+    <FlatList 
+      data={participants}
+      keyExtractor={item => item}
+      renderItem={({ item }) => (
+        <Participant 
+          key={item}
+          name={item} 
+          onRemove={() => handleParticipantRemove(item)}
+        />
+      )}
+      showsVerticalScrollIndicator={false}
+      ListEmptyComponent={() => (
+        <Text style={styles.listEmptytext}>
+          Ninguém chegou no evento ainda? Adicione participantes a sua lsita de presença.
+        </Text>
+      )}
+    />
 
     </View>
   )
